e70a5efce666a96202c019d79426ab9faf962d84
yangdi <muyi137@163.com>
2022-07-29 11:46:09 +0800

    增加插件

---
+++ server/plugin/notify/global/global.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/flipped-aurora/gin-vue-admin/server/plugin/notify/config"
+
+var GlobalConfig_ = &config.DingDing{
+	Url:    "https://oapi.dingtalk.com/robot/send?access_token=10687fd956e563bedaee88743317b908a6338fd17e1f64c6770636989b051acc",
+	Token:  "10687fd956e563bedaee88743317b908a6338fd17e1f64c6770636989b051acc",
+	Secret: "SEC7c481ffd4f83b09b4d1a7d5ef214c83c37ebc9484c023822b3c467c9f8d8d90c",
+}
