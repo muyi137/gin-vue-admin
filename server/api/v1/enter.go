738cf344c39d43bc3d8dfbcfec021aa8c6b2f10a
yangdi <muyi137@163.com>
2022-11-09 11:48:46 +0800

    测

--- server/api/v1/enter.go
+++ server/api/v1/enter.go
@@ -2,12 +2,14 @@ package v1
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/oa"
 	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/system"
 )
 
 type ApiGroup struct {
 	SystemApiGroup  system.ApiGroup
 	ExampleApiGroup example.ApiGroup
+	OaApiGroup      oa.ApiGroup
 }
 
 var ApiGroupApp = new(ApiGroup)
--- server/router/enter.go
+++ server/router/enter.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/router/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/router/oa"
 	"github.com/flipped-aurora/gin-vue-admin/server/router/system"
 )
 
 type RouterGroup struct {
 	System  system.RouterGroup
 	Example example.RouterGroup
+	Oa      oa.RouterGroup
 }
 
 var RouterGroupApp = new(RouterGroup)
